12f68c9aac7f813b1a30bf69a22b60e9eacf9e23
EyuelMathewos <31162254+EyuelMathewos@users.noreply.github.com>
2021-04-09 16:30:08 +0300

    Update root.js

--- server/boot/root.js
+++ server/boot/root.js
@@ -47,7 +47,7 @@ client.connect(function(error) {
     const headers = {
       "Content-Range": `bytes ${0}-${end}/${videoSize}`,
       "Accept-Ranges": "bytes",
-      "Content-Length": ${videoSize},
+      "Content-Length": metadata.length,
       "Content-Type": "video/mp4",
     };
 
