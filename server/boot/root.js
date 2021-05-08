d1048e6100d490ea771476e3eca8fc079a7b5bad
EyuelMathewos <31162254+EyuelMathewos@users.noreply.github.com>
2021-05-08 11:11:18 +0300

    Update root.js

--- server/boot/root.js
+++ server/boot/root.js
@@ -40,11 +40,11 @@ client.connect(function(error) {
 //         if (err) throw err;
 //         metadata = video;
 //        });
-//       db.collection.find({ _id : "60788fb914a3570015e0237b" }).toArray(function(err, video) {
-//           assert.equal(err, null);
-//           console.log('*******Find Video Detial');
-//           console.log(video);
-//        });
+      db.collection('fs.files').find({ _id : "60788fb914a3570015e0237b" }).toArray(function(err, video) {
+          assert.equal(err, null);
+          console.log('*******Find Video Detial');
+          console.log(video);
+       });
         
         
         
