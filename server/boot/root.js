ec90605174895d92d75f7619c8580996e0da3bfb
EyuelMathewos <31162254+EyuelMathewos@users.noreply.github.com>
2021-05-09 12:55:10 +0300

    Update root.js

--- server/boot/root.js
+++ server/boot/root.js
@@ -103,7 +103,7 @@ client.connect(function(error) {
     //     res.set('content-type', 'video/mp4');
     //     res.set('accept-ranges', 'bytes');
     res.setHeader("accept-ranges", "bytes");
-    //res.setHeader("content-length", 3964664);
+    res.setHeader("content-length", 10445411);
     //res.setHeader(headers);
     console.log(headers);
     //     res.status(206);
